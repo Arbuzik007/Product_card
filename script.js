7ff27d972dac7a4d5ffd00189bf5c211656104a1
Arbuzik007 <77507273+Arbuzik007@users.noreply.github.com>
2023-07-16 12:41:33 +0300

    create project

---
+++ script.js
@@ -0,0 +1,66 @@
+var plus = document.querySelectorAll ('.plus');
+var minus = document.querySelectorAll('.minus');
+var number = document.querySelectorAll('.number');
+var i = 0;
+
+plus[i].onclick = up;
+function up() {
+  var count = Number(number[i].innerHTML);
+  number[i].innerHTML = count += 1;
+}
+minus[i].onclick = down;
+function down() {
+  var count = Number(number[i].innerHTML);
+  number[i].innerHTML = count -= 1;	
+}
+
+
+
+/* Когда пользователь нажимает на кнопку,
+переключение между скрытием и отображением раскрывающегося содержимого */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Закройте выпадающее меню, если пользователь щелкает за его пределами
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+
+function burgerMenu(selector) {
+  let menu = $(selector);
+  let button = menu.find('.burger-menu_button', '.burger-menu_lines');
+  let links = menu.find('.burger-menu_link, .burger-menu_link-mini');
+  let overlay = menu.find('.burger-menu_overlay');
+  
+  button.on('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+  
+  links.on('click', () => toggleMenu());
+  overlay.on('click', () => toggleMenu());
+  
+  function toggleMenu(){
+    menu.toggleClass('burger-menu_active');
+    
+    if (menu.hasClass('burger-menu_active')) {
+      $('body').css('overlow', 'hidden');
+    } else {
+      $('body').css('overlow', 'visible');
+    }
+  }
+}
+
+burgerMenu('.burger-menu');
